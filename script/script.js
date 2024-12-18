69b38781e8047f57e3b7c8a44d08ed7abe195624
beva3 <mizabienvenu@gmail.com>
2024-12-18 21:50:29 +0530

    display

--- script/script.js
+++ script/script.js
@@ -24,14 +24,33 @@ const data = [
       "date_joined": "2024-01-15"
     }
 ]
-console.log(data);
+let table_body = document.querySelector('.table-body')
 
 class manage{
     constructor(d){
         this.data = d;
     }
+    getRow(i,d_row){
+      return `
+        <tr>
+          <th scope="row">${i}</th>
+            <td>${d_row.name}</td>
+               <td>${d_row.email}</td>
+            <td>${d_row.role}</td>
+          <td>${d_row.date_joined}</td>
+        </tr>
+      `
+    }
     display(){
+        console.log('--------------------------------');
+        
         console.log("display data...");
+        let index = 1;
+        this.data.forEach((elt) => {
+          table_body.innerHTML += this.getRow(index,elt);
+          index++;  // increment index for next row
+        });
+        console.log('--------------------------------');
     } 
     sort(by_item){
         console.log(`sorting data... by ${by_item}`);
@@ -39,11 +58,14 @@ class manage{
     search(query){
         console.log(`searching data...  ${query}`);   
     }
+    run(){
+      this.display();
+      this.sort('date_joined');
+      this.search('Moderator');  
+    }
 }
 
 const mng = new manage(data);
-mng.display();
-mng.sort('name');
-mng.search('Johnson');
+mng.run();
 
 
